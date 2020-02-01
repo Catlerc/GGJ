60d4607ecb13ae7884210c725227fe3c33238ad5
Mihail Salkov <misha-sal@mail.ru>
2020-02-01 11:55:15 +0500

    Add Starter, CarMove

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public float rotateSpeed;
+    public float forwardSpeed;
+    public GameObject cameraObj;
+    public float cameraOffsetZ;
+    public bool engine;
+
+    void Start()
+    {
+        engine = false;
+    }
+
+    void Update()
+    {
+        if (engine)
+        {
+            Move();
+            CheckRoad();
+            MoveCamera();
+        }
+    }
+
+    void Move()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed, 0);
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
+    }
+
+    void CheckRoad()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 100))
+        {
+            Debug.Log(hit.collider);
+            if (hit.collider.CompareTag("Border"))
+            {
+                engine = false;
+            }
+        }
+    }
+
+    void MoveCamera()
+    {
+        var oldPos = cameraObj.transform.position;
+
+        cameraObj.transform.position = new Vector3(oldPos.x, oldPos.y, transform.position.z + cameraOffsetZ);
+    }
+}
---
+++ Assets/Scripts/Starter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starter : MonoBehaviour
+{
+    public GameObject cameraObj;
+    public Vector3 cameraStartPos, cameraStartRotate;
+    private Vector3 cameraWorkPos, cameraWorkRotate;
+
+    void Start()
+    {
+        cameraWorkPos = cameraObj.transform.position;
+        cameraWorkRotate = cameraObj.transform.rotation.eulerAngles;
+
+        goStartPos();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Return))
+        {
+            StartGame();
+        }
+    }
+
+    void StartGame()
+    {
+        goWorkPos();
+        gameObject.GetComponent<Car>().engine = true;
+    }
+
+    void goWorkPos()
+    {
+        cameraObj.transform.position = cameraWorkPos;
+        cameraObj.transform.rotation = Quaternion.Euler(cameraWorkRotate);
+    }
+
+    void goStartPos()
+    {
+        cameraObj.transform.position = cameraStartPos;
+        cameraObj.transform.rotation = Quaternion.Euler(cameraStartRotate);
+    }
+}
