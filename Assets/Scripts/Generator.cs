1b967e3de08f00ebd61fba63af4219af095b17d8
Catlerc <47296502+Catlerc@users.noreply.github.com>
2020-02-01 10:18:10 +0500

    upgraded map generator

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -3,40 +3,52 @@ using UnityEngine;
 using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
-public class Generator: MonoBehaviour
+public class Generator : MonoBehaviour
 {
-    [NonSerialized]
-    public static float SizeOfTileSide = 8 / 10f;
+    [NonSerialized] public static float SizeOfTileSide = 8 / 10f;
     public GameObject[] prefabs;
     public GameObject bord;
-    [NonSerialized]
-    public readonly int width = 28;
-    public int lineIndex = 0;
+    private readonly int width = 28;
+    private int lineIndex = 0;
+
+    private bool isBroken(Vector2Int pos)
+    {
+        float scale = 1f / 12;
+        float v = Mathf.PerlinNoise(pos.x * scale, pos.y * scale);
+
+        float scale2 = 1f / 10;
+        float v2 = Mathf.PerlinNoise(pos.x * scale2, pos.y * scale2);
+
+        return v > .5f || v2 > .5f;
+    }
+
     public void InstantiateLine()
     {
-        for (var i = 1; i <=width;i++)
+        for (var x = 1; x <= width; x++)
         {
-            switch (i)
+            switch (x)
             {
                 case 1:
-                    bord.InstantiateToMap(new Vector2Int(i,lineIndex),-90);
+                    bord.InstantiateToMap(new Vector2Int(x, lineIndex), -90);
                     break;
-                case int index when index==width:
-                    bord.InstantiateToMap(new Vector2Int(i,lineIndex),90);
+                case int index when index == width:
+                    bord.InstantiateToMap(new Vector2Int(x, lineIndex), 90);
                     break;
                 default:
-                    prefabs[Random.Range(0, prefabs.Length)].InstantiateToMap(new Vector2Int(i,lineIndex), 90*Random.Range(0,4));    
+                    prefabs[isBroken(new Vector2Int(x, lineIndex)) ? 0 : 1]
+                        .InstantiateToMap(new Vector2Int(x, lineIndex), 90 * Random.Range(0, 4));
                     break;
             }
         }
+
         lineIndex++;
     }
-    
+
     //TEST 
 
     public void Start()
     {
-        for (int i=0;i<20;i++)
+        for (int i = 0; i < 100; i++)
             InstantiateLine();
     }
 }
